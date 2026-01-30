655bf1a6356e5a6cbcfc71a32feb675cd852dffe
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2026-01-30 16:10:26 -0300

    implemented the import for houses from CSV

---
+++ app.py
@@ -0,0 +1,196 @@
+import os
+import csv
+import hashlib
+import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+state_map = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+}
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class House(db.Model):
+    id = db.Column(db.String(120), primary_key=True, unique=True)
+    brokered_by = db.Column(db.String(120))
+    status = db.Column(db.String(30))
+    price = db.Column(db.Float)
+    bed = db.Column(db.Integer)
+    bath = db.Column(db.Integer)
+    acre_lot = db.Column(db.Float)
+    street = db.Column(db.String(120))
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    zip_code = db.Column(db.String(120))
+    house_size = db.Column(db.Float)
+    prev_sold_date = db.Column(db.String(120))
+    # code from mapping
+    state_code = db.Column(db.String(10))
+    # calculated columns
+    price_per_acre = db.Column(db.Float)
+    price_per_sq_ft = db.Column(db.Float)
+
+@app.cli.command("init-db")
+def init_db():
+    """Clear existing data and create new tables."""
+    db.create_all()
+    print("Database initialized!")
+
+def to_float(number):
+    try:
+        return float(number)
+    except ValueError:
+        return None
+
+def to_int(number):
+    try:
+        return int(number)
+    except ValueError:
+        return None
+
+def db_optimization(cursor):
+    # Disables rollback log
+    cursor.execute("PRAGMA journal_mode = OFF;")
+    # Doesn't wait for disk write confirmation
+    cursor.execute("PRAGMA synchronous = OFF;")
+    # Uses 1GB of RAM for cache
+    cursor.execute("PRAGMA cache_size = -1000000;")
+    # Prevents other apps from touching DB
+    cursor.execute("PRAGMA locking_mode = EXCLUSIVE;")
+
+def insert_house(cursor, buffer):
+    cursor.executemany(
+        "INSERT OR IGNORE INTO house (brokered_by, status, price, bed, bath, acre_lot, street, city, state, zip_code, house_size, prev_sold_date, state_code, price_per_acre, price_per_sq_ft, id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+        buffer
+    )
+
+@app.cli.command("import-csv")
+def import_csv():
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    db_optimization(cursor)
+
+    csv_file_path = os.path.join(basedir, 'realtor-data.csv')
+
+    if not os.path.exists(csv_file_path):
+        print(f"Error: {csv_file_path} not found.")
+        return
+
+    with open(csv_file_path, mode='r') as file:
+        # skip header
+        file.readline()
+
+        count_added = 0
+        in_batch = 0
+        batch_size = 100000
+        buffer = []
+
+        for raw_line in file:
+            raw_line = raw_line.strip()
+            if not raw_line:
+                continue
+
+            row_hash = hashlib.sha256(raw_line.encode('utf-8')).hexdigest()
+
+            columns = next(csv.reader([raw_line]))
+
+            state = columns[8]
+            state_code = state_map.get(state, "")
+            columns.append(state_code)
+            
+            price = to_float(columns[2])
+            if price is None or price <= 0:
+                continue
+            columns[2] = price
+
+            acre_lot = to_float(columns[5]) or 0
+            columns[5] = acre_lot
+            price_per_acre = 0 if acre_lot == 0 else price / acre_lot
+            columns.append(price_per_acre)
+
+            house_size = to_float(columns[10]) or 0
+            columns[10] = house_size
+            price_per_sq_ft = 0 if house_size == 0 else price / house_size
+            columns.append(price_per_sq_ft)
+
+            columns.append(row_hash)
+            buffer.append(columns)
+
+            count_added += 1
+            in_batch += 1
+            if in_batch == batch_size:
+                insert_house(cursor, buffer)
+                conn.commit()
+                print("completed %.2f %%" % (100 * count_added / 2300000))
+                in_batch = 0
+                buffer = []
+
+        if buffer:
+            insert_house(cursor, buffer)
+            conn.commit()
+            print("completed %.2f %%" % (100 * count_added / 2300000))
+            in_batch = 0
+            buffer = []
+
+        print(f"Import finished: {count_added} added")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
